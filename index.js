3b26870555ae788ed7416c95e6b8891123149cc9
Frostee <caleb.froese@gmail.com>
2016-10-12 22:40:17 +1030

    Response sends the valid cookie in JSON format to the correct login

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '8ab36MQ',
-    database: 'wmb'
+    database: 'wmb',
+    multipleStatements: true
 });
 
 var app = express();
@@ -23,21 +24,43 @@ app.get('/api/v1/account/validate', function (req, res) {
      * on behalf of the client.
      * 
      * Address should be:
-     * /api/v1/account/validate?id=int&pwd=string
+     * /api/v1/account/validate?usr=int&pwd=string
      */
 
-    var accountId = req.query.id;
+    var accountUsername = req.query.usr;
     var accountPassword = req.query.pwd;
 
-    if (accountId && accountPassword) {
-        var sqlQuery = 'SELECT * FROM accounts WHERE accountId = ? AND accountPassword = ?';
-        connection.query(sqlQuery, [accountId, accountPassword], function (err, rows, fields) {
-            if (rows) {
-                // Send a JSON response containing the applicable data
-                res.setHeader('Content-Type', 'application/json');
-                res.write(JSON.stringify(rows));
-                res.end();
+    if (accountUsername && accountPassword) {
+        var sqlQuery = 'SELECT accountId FROM accounts WHERE accountUsername = ? AND accountPassword = ?';
+        connection.query(sqlQuery, [accountUsername, accountPassword], function (err, rows, fields) {
+            if (err) {
+                // An error occurred within the query
+                console.log("An error occured: " + err);
+            } else if (rows.length) {
+                // Query returned a valid reponse
+
+                // Get data from the query response
+                var accountId = rows[0].accountId;
+
+                // Set the session
+                var token = Math.random().toString(36).substring(7);
+                
+                connection.query('UPDATE accounts SET accountToken = ? WHERE accountId = ?', [token, accountId], function (err, rows, fields) {
+                    if (err) {
+                        console.log("An error occured: " + err);
+                    } else {
+                        // Create a JSON response
+                        var JSONresponse = '{"token":' +  token + '}';
+
+                        // Send the JSON response containing the token
+                        res.setHeader('Content-Type', 'application/json');
+                        res.write(JSONresponse);
+                        res.end();
+                    }
+                });
+
             } else {
+                // Query returned no rows
                 res.end("Empty");
             }
         });
